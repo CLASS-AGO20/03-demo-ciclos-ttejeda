d0ec0b09157e96f854c38948491399ca547377bf
Tirzo <ttejeda@ucol.mx>
2020-12-04 17:00:22 -0600

    implemented contarImparesFor()

--- src/main.js
+++ src/main.js
@@ -10,8 +10,22 @@ export default class App {
 
       return suma;
     }
+
+    contarImparesFor(inicio, fin){
+        let impares = 0;
+
+        for (let i = inicio; i <= fin; i = i + 1){
+            if (i % 2 != 0){
+                impares = impares + 1;
+            }
+        }
+
+      return impares;
+    }
 }
 
 let app = new App();
 
 console.log(app.sumarParesFor());
+
+console.log(app.contarImparesFor(5, 11));
