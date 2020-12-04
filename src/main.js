e36d1eef64d23282708aecbf292fca24de66451d
Tirzo <ttejeda@ucol.mx>
2020-12-04 15:27:51 -0600

    implmented sumarParesFor()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
-export default class App {}
+export default class App {
+
+    sumarParesFor(){
+        let suma = 0;
+
+        for (let i = 1; i <= 20; i = i + 1){
+            if (i % 2 == 0){
+                suma = suma + i;
+            }
+        }
+
+      return suma;
+    }
+}
 
 let app = new App();
+
+console.log(app.sumarParesFor());
