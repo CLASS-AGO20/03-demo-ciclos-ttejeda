5e4a87422a06788844f87fcacf5ba46fafef066b
Tirzo <ttejeda@ucol.mx>
2020-12-04 16:01:46 -0600

    optimized sumarParesFor()

--- src/main.js
+++ src/main.js
@@ -3,11 +3,10 @@ export default class App {
     sumarParesFor(){
         let suma = 0;
 
-        for (let i = 1; i <= 20; i = i + 1){
-            if (i % 2 == 0){
-                suma = suma + i;
-            }
+        for (let i = 0; i <= 20; i = i + 2){
+            suma = suma + i;
         }
+        
 
       return suma;
     }
