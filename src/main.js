a47c176403b37be22489c78d8369a8214992b122
Tirzo <ttejeda@ucol.mx>
2020-12-04 18:33:54 -0600

    implemented sumarParesWhile()

--- src/main.js
+++ src/main.js
@@ -28,6 +28,21 @@ export default class App {
 
       return impares;
     }
+
+    sumarParesWhile(){
+        let i = 1;
+        let suma = 0;
+
+        while(i <= 20){
+            if(i & 2 == 0){
+                suma = suma + 1;
+            }
+
+            i = i + 1;
+        }
+
+      return suma;
+    }
 }
 
 let app = new App();
@@ -35,3 +50,5 @@ let app = new App();
 console.log(app.sumarParesFor());
 
 console.log(app.contarImparesFor(5, 11));
+
+console.log(app.sumarParesWhile());
